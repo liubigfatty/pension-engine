4ba037b12ce02d959fd1c9cd04e9899e7dcdaef8
liubigfatty <liubigfatty@github.com>
2026-06-17 08:58:41 +0800

    fix: urlCheck=false+关闭hotReload+引擎改用responseType

--- miniprogram/utils/engine.js
+++ miniprogram/utils/engine.js
@@ -1,7 +1,5 @@
 /**
  * 引擎远程加载工具
- * 通过 wx.request 从 GitHub Pages 远程加载（域名已配白名单）
- * 缓存结果，只加载一次
  */
 const ENGINE_URL = 'https://liubigfatty.github.io/pension-calculator/js/pension-engine-browser.js'
 let _engine = null
@@ -11,11 +9,10 @@ function loadEngine() {
   return new Promise(function(resolve, reject) {
     wx.request({
       url: ENGINE_URL,
-      dataType: 'text',
+      responseType: 'text',
       success(res) {
         try {
           var mod = { exports: {} }
-          // 在沙箱中执行，捕获 module.exports
           new Function('module', 'exports', res.data)(mod, mod.exports)
           _engine = mod.exports
           resolve(_engine)
